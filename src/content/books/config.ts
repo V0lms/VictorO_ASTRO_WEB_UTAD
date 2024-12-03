cc8e125831260c62da4b8ec6508c0fd3a653d2a8
Victor <victorolmosmartin@gmail.com>
2024-12-03 10:10:32 +0100

    update

---
+++ src/content/books/config.ts
@@ -0,0 +1,14 @@
+import { defineCollection, z } from "astro:content";
+
+const books = defineCollection({
+    schema: z.object({
+        title: z.string(),
+        author: z.string(),
+        img: z.string(),
+        readtime: z.number(),
+        description: z.string(),
+        buy: z.string().url(),
+    })
+})
+
+export const collection = { books }
